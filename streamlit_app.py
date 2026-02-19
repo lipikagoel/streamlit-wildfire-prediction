95649d301b83e632edd8eb90490b03cf37ea9f4a
Hayden Samala <hsamala998@gmail.com>
2026-02-18 16:40:47 -0800

    Update app description and replace greeting message

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title('California Wildfire Prediction App')
 
-st.write('Hello world!')
+st.info('This is an app which if you input certain characteristics of weather, precipitation, locaiton, etc. it should display which areas within California are most at risk for wildfires')
