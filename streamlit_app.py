8d7a32829157f07d593f6a305d76066904e236a5
Hayden Samala <hsamala998@gmail.com>
2026-02-18 16:45:44 -0800

    Implement sidebar for input features
    Added sidebar for user input features including location and date.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,9 @@ import streamlit as st
 st.title('California Wildfire Prediction App')
 
 st.info('This is an app which if you input certain characteristics of weather, precipitation, locaiton, etc. it should display which areas within California are most at risk for wildfires')
+
+with st.sidebar:
+  st.header('Input features')
+  location = st.selectbox
+  date = st.date_input(value, format = "MM.DD.YYYY")
+  
