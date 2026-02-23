f25805a99884e2c0e5e0b7759969408b24308771
Hayden Samala <hsamala998@gmail.com>
2026-02-22 23:15:22 -0800

    Remove sidebar input features from app
    Removed sidebar input features section from the app.

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,10 +7,5 @@ import pandas as pd
 st.title('California Wildfire Prediction App')
 
 st.info('This is an app which if you input certain characteristics of weather, precipitation, locaiton, etc. it should display which areas within California are most at risk for wildfires')
-
-with st.sidebar:
-  st.header('Input features')
-  #
-  date = st.date_input("Enter the Date:", min_value=date(2020, 1, 1), max_value=date(2025, 12, 31))
   
   
