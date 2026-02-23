83eb7379e0e6a6a9d2e86d8677e685e548458a13
Hayden Samala <hsamala998@gmail.com>
2026-02-22 23:19:31 -0800

    Fix month_names dictionary syntax error

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import pandas as pd
 API_URL = "https://app-wildfire-prediction-gvp5tpcymq2uae4qndccr9.streamlit.app/"
 
 month_names = {
-  1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June", 7: "July", 8:"August", 9: "September", 10:"October", 11;"November", 12"December"}
+  1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June", 7: "July", 8:"August", 9: "September", 10:"October", 11; "November", 12: "December"}
 
 risk_assignment = {"Low": "#2ecc71", "Medium": "#f39c12", "High": "#e74c3c"}
 
