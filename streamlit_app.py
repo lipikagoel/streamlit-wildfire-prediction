6c76af7651a659d68e85c9f6ba50d094134b2e37
Hayden Samala <hsamala998@gmail.com>
2026-02-22 23:19:11 -0800

    Initialize API URL and month names
    Added API URL and month names for wildfire prediction.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,14 @@ import requests
 import plotly.express as px
 import pandas as pd
 
+API_URL = "https://app-wildfire-prediction-gvp5tpcymq2uae4qndccr9.streamlit.app/"
+
+month_names = {
+  1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June", 7: "July", 8:"August", 9: "September", 10:"October", 11;"November", 12"December"}
+
+risk_assignment = {"Low": "#2ecc71", "Medium": "#f39c12", "High": "#e74c3c"}
+
+st.set_page_config(page_title = "Cali Wild Risk", layout = "wide")
 
 st.title('California Wildfire Prediction App')
 
