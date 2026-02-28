47b1b9863d8c98c2b773960b84685347188ef933
Hayden Samala <hsamala998@gmail.com>
2026-02-28 13:16:21 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,26 +26,58 @@ def reset():
 with st.sidebar:
     st.title("Adjust the Risk Factors:") # sidebar title
     #fire = st.selectbox("Fire:",["A","B","C"]) # I don't think we need this, we just a user to input the data
-    latitude = st.slider("Latitude", 32.5, 42.0,value=34.0689,step = 0.01, format="%.2f", key = "lat_input") # changed these so that they only have 2 decimal points
-    longitude = st.slider("Longitutde", -124.4, -114.1,value=-118.4452,step = 0.01, format="%.2f", key = "lon_input") # changed these so that they only have 2 decimal points
-    acq_hour = st.slider("Acquired Hour:", 0, 23, 12, key = "hour_input")
+    
+    latitude = st.slider("Latitude", 
+                         32.5, 42.0,
+                         step = 0.01, 
+                         format="%.2f", 
+                         value=st.session_state.get("lat_input",34.0689),
+                         key = "lat_input") # changed these so that they only have 2 decimal points
+    
+    longitude = st.slider("Longitutde", 
+                          -124.4, -114.1,
+                          step = 0.01, 
+                          format="%.2f", 
+                          value=st.session_state.get("lon_input",-118.4452),
+                          key = "lon_input") # changed these so that they only have 2 decimal points
+    
+    acq_hour = st.slider("Acquired Hour:", 0, 23, 12, value = st.session_state.get("hour_input", 12), key = "hour_input")
+    
     st.markdown("---")
-    wx_tavg_c = st.number_input("Average Daily Temperature (C)", step=1, format="%d", key = "temp_input")
-    wx_prcp_mm= st.number_input("Total Daily Precipitation (mL)",step = 0.01, format="%.2f", key = "prec_input") # changed these so that they only have 2 decimal points
-    wx_wspd_ms= st.number_input("Wind Speed (m/s)",step = 0.01, format="%.2f", key = "wind_input") # changed these so that they only have 2 decimal points
+    
+    wx_tavg_c = st.number_input("Average Daily Temperature (C)", step=1, format="%d", 
+                                value = st.session_state.get("temp_input", 12), 
+                                key = "temp_input")
+    
+    wx_prcp_mm= st.number_input("Total Daily Precipitation (mm)",
+                                step = 0.01, 
+                                format="%.2f", 
+                                value = st.session_state.get("prec_input", 0),
+                                key = "prec_input") # changed these so that they only have 2 decimal points
+    
+    wx_wspd_ms= st.number_input("Wind Speed (m/s)", 
+                                step = 0.01, 
+                                format="%.2f", 
+                                key = "wind_input") # changed these so that they only have 2 decimal points
+    
     st.markdown("---")
-    lf_evc= st.slider("Vegetation Cover (%)", 0, 100, 50, key = "veg_cov_input")
-    lf_evh= st.slider("Vegetation Height (cm)", 0, 1000, 100, key = "veg_hei_input")
+    lf_evc= st.slider("Vegetation Cover (%)", 0, 100, 50, 
+                      value = st.session_state.get("veg_cov_input", 0),
+                      key = "veg_cov_input")
+    
+    lf_evh= st.slider("Vegetation Height (cm)", 0, 1000, 100, 
+                      value = st.session_state.get("veg_hei_input", 0),
+                      key = "veg_hei_input")
+    
     # evt_fuel_n= st.selectbox("Fuel Type", le.classes_)
-    #st.button("Apply Filters")
 
 col_btn1, col_btn2 = st.columns(2)
 
 with col_btn1:
-    clicked = st.button("Predict", use_container_width = "True")
+    clicked = st.button("Predict", type = "primary" use_container_width = "True")
 
 with col_btn2:
-    if st.button("Clear Values", type = "primary", use_container_width=True):
+    if st.button("Clear Values", use_container_width=True):
         reset()
     
 st.subheader("Specific Location Risk Assessment:")
@@ -96,7 +128,7 @@ st.dataframe(df2.style.format("{:.2f}"), use_container_width=True)
 
 st.subheader("Overall State Risk Assessment:")
 
-if st.button:
+if st.button("Generate Statewide Heatmap", use_container_width=True):
     with st.spinner("Processing..."):
         lats = np.linspace(32.5, 42.0)
         longs = np.linspace(-124.4, -114.1)
