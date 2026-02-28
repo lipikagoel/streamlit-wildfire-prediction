8be6165d9a454fceee0a2e5ab2be2668b39d6b12
Hayden Samala <hsamala998@gmail.com>
2026-02-28 11:59:38 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,8 +69,26 @@ st.map(pd.DataFrame({"lat": [latitude], "lon": [longitude]}))
 '''
 if the button Predict Wildfire Risk is pressed runs the joblib files, then for that specific point what is the risk. Can't do that without model
 '''
+# Creating a map so shows state-wide risk given certain conditions
 if st.button:
     lats = np.linspace(32.5, 42.0)
     longs = np.linspace(-124.4, -114.1)
     grid_points = [(lats,longs) for lat in lats for lon in longs]
+
+    grid_df[acq_hour] = hour
+    grid_df[wx_tavg_c] = temp
+    grid_df[wx_prcp_mm] = precip
+    grid_df[wx_wspd_ms] = wind_speed
+    grid_df[lf_evc] = veg_cover 
+    grid_df[lf_evh] = veg_height
+
+    grid_df = grid_df[feature_names]
+    probabilities = model.predict_proba(grid_df)[:, 1]
+
+    grid_df['risk score'] = probabilities
+
+    high_risk_map = grid_df[grid_df['risk_score'] > 0.5]
+
+    st.map(high_risk_map)
+    
     
