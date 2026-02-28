3f6a92cf4847a1ada8c1b956f4e8a3d9739f5acf
Hayden Samala <hsamala998@gmail.com>
2026-02-28 12:08:23 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,3 @@
-"""
-Hey, here are you instructions for what I want the streamlit page to have. I would like the page to have a fairly
-simply UI, but feel free to customize as you go along.
-
-Add a sidebar where a user can input information for the following fields:
-'fire', 'latitude', 'longitude', 'acq_hour', 'wx_tavg_c', 'wx_prcp_mm', 'wx_wspd_ms', 'lf_evc', 'lf_evh', 'EVT_FUEL_N'
-
-Consider this list as a temporary placeholder as the ML team decides what columns they are going to be using for the RCF
-model.
-
-If you need any help please refer to the folder labeled Example which houses an example streamlit script as well as
-the associated RCF model as well as the requisite PKL files needed for the model to work.
-
-Let me know within the next couple days how far you get and feel free to delete this block of comments once you are
-done
-
-Also for you to be able to deploy the app itself feel free to fork the repository and test it out on your own.
-"""
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -46,6 +28,7 @@ with st.sidebar:
     # evt_fuel_n= st.selectbox("Fuel Type", le.classes_)
     #st.button("Apply Filters")
 
+# DataFrame Setup
 data = {
     "Latitude": [latitude],
     "Longitude": [longitude],
@@ -71,6 +54,8 @@ st.map(pd.DataFrame({"lat": [latitude], "lon": [longitude]}))
 # Creating a map so shows state-wide risk given certain conditions
 
 st.markdown("---")
+
+# Second Dataframe Setup
 
 data2 = {
     "Hour in Day" : [acq_hour],
