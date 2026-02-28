05277aa95e15449c0cc827174b7a1daab889db56
lipikagoel <lipika.lifeisgood@gmail.com>
2026-02-28 08:49:42 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,3 +16,35 @@ done
 
 Also for you to be able to deploy the app itself feel free to fork the repository and test it out on your own.
 """
+
+import streamlit as st
+import datetime
+
+## PAGE SETUP
+st.set_page_config(page_title="TEST California Cities Map")
+st.subheader("California Wildfire Prediction")
+st.markdown("Data from: `...`.")
+
+## SIDEBAR
+with st.sidebar:
+    st.title("Risk Factors") # sidebar title
+    fire = st.selectbox("Fire:",["A","B","C"]) # dropdown selection
+    latitude = st.slider("Latitude", -90, 90) # slider selection
+    latitude = st.slider("Longitutde", -90, 90) # slider selection
+    acq_hour = st.time_input("Acquired Hour:", "12:00")
+    wx_tavg_c = st.number_input("Average Daily Temperature (C)", step=1, format="%d")
+    wx_prcp_mm= st.number_input("Total Daily Precipitation (mL)")
+    wx_wspd_ms= st.number_input("Wind Speed (m/s)")
+    lf_evc= st.number_input("Density of Vegetation")
+    lf_evh= st.selectbox("Amount of Vegetation", ["Low", "High"])
+    evt_fuel_n= st.selectbox("Fuel Identifier", ["Option 1", "Option 2"])
+    st.markdown("---") # Add a horizontal rule
+    st.button("Apply Filters")
+
+# st.write(f"You selected: {fire} and {latitude}") # write what options have been selected (onto main page)
+
+# selected_page = st.sidebar.radio('Go to', ['Page 1', 'Page 2', 'Page 3']) # single-select multiple choice
+
+# st.sidebar.header('Filters')
+
+# date_range = st.sidebar.date_input('Select Date Range')
